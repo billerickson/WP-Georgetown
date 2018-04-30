a921d7d1cfee81f3a62321a4cb352250a0791ea9
Bill Erickson <bill.erickson@gmail.com>
2018-04-30 08:19:13 -0500

    comment styling

--- wp-content/themes/wpgeorgetown-2018/comments.php
+++ wp-content/themes/wpgeorgetown-2018/comments.php
@@ -24,16 +24,7 @@ if ( post_password_required() ) {
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'ea' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
-
+		<h2 class="comments-title">Comments</h2>
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'ea' ); ?></h2>
